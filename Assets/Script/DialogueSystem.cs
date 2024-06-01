eb26d9751a0b9c5fe2cac2e70ca4f48244f2beb4
Aloiszz <a.tupinon@rubika-edu.com>
2024-06-01 14:30:48 +0200

    Dialogue system

---
+++ Assets/Script/DialogueSystem.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using TMPro;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI diaglogueText;
+    public List<Scenario> scenarios;
+
+    public static DialogueSystem Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PlayScenario(0));
+    }
+
+    public IEnumerator PlayScenario(int scenarioIndex)
+    {
+        foreach (var dialogue in scenarios[scenarioIndex].dialogues)
+        {
+            diaglogueText.text = dialogue.text;
+            yield return new WaitForSeconds(dialogue.timeDisplayed);
+        }
+    }
+}
+
+
+[System.Serializable]
+public class Scenario
+{
+    public List<Dialogue> dialogues;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    [HorizontalLine(color: EColor.Red)]
+    [ResizableTextArea]
+    public string text;
+    public float timeDisplayed;
+}
